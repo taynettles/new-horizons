06f714fe8aeed5db4d677b7e6898f6a122dc9fc7
Taylen Nettles <tay@Taylens-MacBook-Pro.local>
2019-12-24 19:18:56 -0600

    Updates

---
+++ precache-manifest.f3cdf014caa1d32e76d9f739c03ff4a1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f63b50d35abd67cefdec810ed2ed8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaef07f2534c85414514",
+    "url": "/static/css/main.a1d3ee80.chunk.css"
+  },
+  {
+    "revision": "6372287f96924743997e",
+    "url": "/static/js/2.272d94dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.272d94dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aaef07f2534c85414514",
+    "url": "/static/js/main.c07aa260.chunk.js"
+  },
+  {
+    "revision": "94d800de2230648c159d",
+    "url": "/static/js/runtime-main.90fd33df.js"
+  },
+  {
+    "revision": "796b643a146db4b34af5f263f2b19e4a",
+    "url": "/static/media/FinkHeavy.796b643a.ttf"
+  },
+  {
+    "revision": "2e77a25e8c6981ba6c9de8d5166929e6",
+    "url": "/static/media/acnh-logo.2e77a25e.png"
+  },
+  {
+    "revision": "ec009472676a17c3b91d5b4f673311e8",
+    "url": "/static/media/bg1.ec009472.jpg"
+  }
+]);
